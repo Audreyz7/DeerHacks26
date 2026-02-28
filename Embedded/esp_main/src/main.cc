07a568c9addcd1b2b7b2b2e7ced8869321e420fe
Jack Nguyen <J4ck.tf@gmail.com>
2026-02-28 16:12:01 -0500

    Speaker setup

--- Embedded/esp_main/src/main.cc
+++ Embedded/esp_main/src/main.cc
@@ -11,16 +11,32 @@
 #define TFT_SDA 23
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_A0, TFT_RST);
 
+#define AUDIO_PIN 25   
+
 void setup() {
+    Serial.begin(115200);
+
+    //screen
     SPI.begin(TFT_SDK, 2, TFT_SDA, TFT_CS);
 
     tft.initR(INITR_GREENTAB);
     tft.setRotation(0);
 
     tft.fillScreen(ST77XX_BLACK); delay(300);
+    tft.fillScreen(ST77XX_WHITE); 
+
+    //speaker
+    ledcAttach(AUDIO_PIN, 2000, 8); 
+    ledcWriteTone(AUDIO_PIN, 600); 
 }
 
 void loop() {
-    tft.fillScreen(ST77XX_BLACK); delay(300);
-    tft.fillScreen(ST77XX_WHITE); delay(300);
+    ledcWrite(AUDIO_PIN, 128);
+    Serial.println("on");
+    delay(500);
+
+
+    ledcWrite(AUDIO_PIN, 0);
+    Serial.println("off");
+    delay(1000);
 }

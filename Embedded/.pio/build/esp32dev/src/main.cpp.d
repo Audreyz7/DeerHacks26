5b8af0de16d17d3922b33d3cd4daaa98b60c443f
Jack Nguyen <J4ck.tf@gmail.com>
2026-02-28 09:28:36 -0500

    Working screen

---
+++ Embedded/.pio/build/esp32dev/src/main.cpp.d
@@ -0,0 +1,62 @@
+.pio/build/esp32dev/src/main.cpp.o: src/main.cpp \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/arduino.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp_arduino_version.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp8266-compat.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/stdlib_noniso.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/binary.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/extra_attr.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-matrix.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-uart.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-gpio.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-touch.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-touch-ng.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-dac.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-adc.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-i2c.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-ledc.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-rmt.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-sigmadelta.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-timer.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-bt.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-psram.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-rgb-led.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-cpu.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-hosted.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/WCharacter.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/WString.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Stream.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Printable.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/IPAddress.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Client.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Server.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Udp.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/HardwareSerial.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBCDC.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Esp.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Arduino.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/freertos_stats.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/io_pin_remap.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Arduino.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h \
+ .pio/libdeps/esp32dev/Adafruit\ GFX\ Library/Adafruit_GFX.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/Print.h \
+ .pio/libdeps/esp32dev/Adafruit\ GFX\ Library/gfxfont.h \
+ .pio/libdeps/esp32dev/Adafruit\ BusIO/Adafruit_I2CDevice.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/libraries/Wire/src/Wire.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-log.h \
+ C:/Users/nguye/.platformio/packages/framework-arduinoespressif32/cores/esp32/HardwareI2C.h \
+ .pio/libdeps/esp32dev/Adafruit\ BusIO/Adafruit_SPIDevice.h \
+ .pio/libdeps/esp32dev/Adafruit\ ST7735\ and\ ST7789\ Library/Adafruit_ST7735.h \
+ .pio/libdeps/esp32dev/Adafruit\ ST7735\ and\ ST7789\ Library/Adafruit_ST77xx.h \
+ .pio/libdeps/esp32dev/Adafruit\ GFX\ Library/Adafruit_SPITFT.h \
+ .pio/libdeps/esp32dev/Adafruit\ GFX\ Library/Adafruit_GFX.h \
+ .pio/libdeps/esp32dev/Adafruit\ GFX\ Library/Adafruit_SPITFT_Macros.h
